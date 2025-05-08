d1df7884f02c01e1ef63efcd26e7dc80f83d1b23
Antonio <antonio.sluzbeni@gmail.com>
2025-05-08 15:15:07 +0200

    Import animation and animate buttun

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { AnimatePresence } from 'framer-motion';
+import { useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 import NewChallenge from './NewChallenge.jsx';
 
-export default function Header() {
+export default function Header({ foo }) {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
 
   function handleStartAddNewChallenge() {
@@ -14,17 +14,24 @@ export default function Header() {
     setIsCreatingNewChallenge(false);
   }
 
+
+
   return (
     <>
       <AnimatePresence>
         {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
       </AnimatePresence>
 
-      <header id="main-header">
+      <header id='main-header'>
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          transition={{ type: 'spring', stiffness: 500 }}
+          onClick={handleStartAddNewChallenge}
+          className='button'
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
