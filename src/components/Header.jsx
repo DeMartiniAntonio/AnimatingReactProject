0b205d8cfc11fe568a890465106cd03a23c0a73a
Antonio <antonio.sluzbeni@gmail.com>
2025-05-12 20:30:26 +0200

    New chellenges

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
 import NewChallenge from './NewChallenge.jsx';
 
-export default function Header({ foo }) {
+export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
 
   function handleStartAddNewChallenge() {
@@ -14,21 +14,19 @@ export default function Header({ foo }) {
     setIsCreatingNewChallenge(false);
   }
 
-
-
   return (
     <>
       <AnimatePresence>
         {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
       </AnimatePresence>
 
-      <header id='main-header'>
+      <header id="main-header">
         <h1>Your Challenges</h1>
         <motion.button
           whileHover={{ scale: 1.1 }}
           transition={{ type: 'spring', stiffness: 500 }}
           onClick={handleStartAddNewChallenge}
-          className='button'
+          className="button"
         >
           Add Challenge
         </motion.button>
